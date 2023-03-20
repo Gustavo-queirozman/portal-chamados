8aaf47c2ef7453ec573f0adeefaeb14c8e52b0e8
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-03-20 14:36:35 -0300

    Update Conttroller

--- app/Http/Controllers/MudarSenhaController.php
+++ app/Http/Controllers/MudarSenhaController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class MudarSenhaController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         return view('mudarSenha');
     }    
 
-    public function mudarSenha(){
-        
+    public function mudarSenha(Request $request){
+        $novaSenha = $request->get('novaSenha');
+        $confirmarSenha = $request->get('confirmarSenha');
+
+        if ($novaSenha == $confirmarSenha) {
+            session_start();
+            $user = new User();
+            $usuario = $_SESSION['usuario'];
+            $user->where('usuario', $usuario)->update(['senha' => $novaSenha]);
+        } else {
+            echo "senhas distintas";
+        }
     }
 }
